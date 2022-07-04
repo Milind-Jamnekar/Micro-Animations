caa1cfbc02770d76aa981064664dbd67a88019dd
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-07-04 21:08:58 +0530

    notification animation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const tl = gsap.timeline({ defualt: { duration: 0.75, ease: "Power3.out" } });
 
 tl.fromTo(".container", { scale: 1 }, { scale: 1.4 });
 
+// Home Button animation
 const home = document.querySelector(".home");
 
 gsap.set(".feather", { scale: 0, transformOrigin: "center" });
@@ -21,5 +22,30 @@ home.addEventListener("click", () => {
     ".left-feather",
     { x: 0, y: -5, scale: 0 },
     { x: 12, y: 20, scale: 1.4, stagger: 0.2, duration: 1 }
+  );
+});
+
+//Notification button animation
+const notification = document.querySelector(".notification");
+gsap.set(".bell", { transformOrigin: "top center" });
+gsap.set(".ringer", { transformOrigin: "top center" });
+gsap.set(".wave", { opacity: 0, transformOrigin: "bottom center" });
+
+notification.addEventListener("click", () => {
+  gsap.fromTo(
+    ".bell",
+    { rotate: -5 },
+    { rotate: 0, duration: 2, ease: "elastic.out(5,0.2)" }
+  );
+  gsap.fromTo(
+    ".ringer",
+    { rotate: -3, x: 0.5 },
+    { rotate: 0, duration: 2, x: 0, ease: "elastic.out(5,0.2)" }
+  );
+
+  gsap.fromTo(
+    ".wave",
+    { scale: 0, opacity: 1 },
+    { scale: 1.5, opacity: 0, duration: 1 }
   );
 });
